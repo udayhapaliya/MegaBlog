5cd097462c2fe7423b1e096b859bf7fb20e21c34
Uday Hapaliya <udayhapaliya24@gmail.com>
2026-09-13 17:44:58 +0530

    feat: add initial project structure with authentication and routing
    - Added favicon and icons SVG files for branding.
    - Implemented App component with authentication handling using Appwrite.
    - Created authService for managing user authentication.
    - Set up Redux store with authSlice for managing authentication state.
    - Developed reusable components: Button, Container, Input, Logo, Header, Footer, and Logout button.
    - Integrated Tailwind CSS for styling.
    - Configured Vite for development with React and Tailwind CSS.
    - Added configuration file for Appwrite settings.

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth.js'
+import { Header } from './components/Header/Header.jsx'
+import { Footer } from './components/Footer/Footer.jsx'
+import { login, logout } from './store/authSlice.js'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch(); 
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        }
+        else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./Header/Header.jsx"
+import Footer from "./Footer/Footer.jsx"
+import Container from "./Container/Container.jsx"
+import Logo from "./Logo.jsx";
+import LogoutBtn from './Header/LogoutBtn.jsx'
+
+export { Header, Footer, Container, Logo, LogoutBtn };
